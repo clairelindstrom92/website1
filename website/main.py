22b10c4c2bacd770c6cb20f229908512b2b722dd
clairelindstrom92 <90209948+clairelindstrom92@users.noreply.github.com>
2023-03-13 19:37:55 -0400

    Add files via upload

--- website/main.py
+++ website/main.py
@@ -22,21 +22,21 @@ with open('static/CommonPasswords.txt', 'r') as f:
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    """Login function."""
-    if request.method == 'POST':
-        username = request.form.get('login_username')
-        password = request.form.get('login_password')
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not check_password_hash(user.password, form.password.data):
+            # Log the failed login attempt
+            app.logger.warning(
+                f'Failed login attempt for user {form.username.data} from IP address {request.remote_addr}.')
 
-        for user in user_database:
-            if username == user.username and password == user.password:
-                session['logged_in'] = True
-                session['username'] = username
-                return redirect(url_for('home'))
-
-        flash('Incorrect username or password.')
-        logging.info(f'Login failure for username: {username}')
-
-    return render_template('templates/login.html')
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
 
 @app.route('/logout', methods=['GET'])
@@ -45,6 +45,53 @@ def logout():
     session.clear()
     return redirect(url_for('login'))
 
+
+@app.route('/updatepassword', methods=['GET', 'POST'])
+@login_required
+def updatepassword():
+    if request.method == 'POST':
+        current_password = request.form['current_password']
+        new_password = request.form['new_password']
+
+        # Verify that the current password matches the one stored in the database
+        if check_password_hash(current_user.password, current_password):
+            # Validate the new password
+            if validate_password(new_password):
+                # Hash the new password and store it in the database
+                hashed_password = generate_password_hash(new_password)
+                current_user.password = hashed_password
+                db.session.commit()
+                flash('Your password has been updated.')
+                return redirect(url_for('index'))
+            else:
+                flash(
+                    'Your password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number.')
+        else:
+            flash('Incorrect current password.')
+
+    return render_template('updatepassword.html')
+
+
+
+
+
+def get_common_passwords():
+    with open('CommonPasswords.txt') as f:
+        common_passwords = [line.strip() for line in f]
+    return common_passwords
+
+def validate_password(password):
+    # Check password length and complexity
+    if len(password) < 8 or not any(char.isdigit() for char in password) or not any(
+            char.isupper() for char in password) or not any(char.islower() for char in password):
+        return False
+
+    # Check if password is a common password
+    common_passwords = get_common_passwords()
+    if password in common_passwords:
+        return False
+
+    return True
 
 @app.route('/', methods=['GET'])
 def index():
